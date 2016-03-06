1a2a4d2fff8452968c91a7f5b9a5e4c5af3f533d
Jeff Lyon <jeff@albatrossdigital.com>
2016-03-06 12:02:51 -0800

    Refining, adding endpoints for initial release of the proudcity api

--- proudpack.php
+++ proudpack.php
@@ -4,85 +4,177 @@
  * Plugin Name: ProudPack
  * Plugin URI:  http://en.obenland.it/wp-proud-search/#utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-proud-search
  * Description: Provides title suggestions while typing a search query, using the built in jQuery suggest script.
- * Version:     2.1.0
- * Author:      Konstantin Obenland
+ * Version:   2.1.0
+ * Author:    Konstantin Obenland
  * Author URI:  http://en.obenland.it/#utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-proud-search
  * Text Domain: wp-proud-search
  * Domain Path: /lang
- * License:     GNU General Public License v2 or later
+ * License:   GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
+namespace Proudpack;
+
 if ( defined('WP_CLI') && WP_CLI ) {
-    include __DIR__ . '/cli/proudpack-phone-home.php';
+  include __DIR__ . '/cli/proudpack-phone-home.php';
 }
 
-add_action( 'rest_api_init', function () {
-	  register_rest_route( 'wp/v2', '/menu', array(
-		    'methods' => 'GET',
-		    'callback' => 'rest_get_plugins',
-		    /*'permission_callback' => function () {
-		        return current_user_can( 'activate_plugins' );
-		    }*/
-	  ) );
-	  register_rest_route( 'wp/v2', '/plugins', array(
-		    'methods' => 'GET',
-		    'callback' => 'rest_get_plugins',
-		    /*'permission_callback' => function () {
-		        return current_user_can( 'activate_plugins' );
-		    }*/
-	  ) );
-	  register_rest_route( 'wp/v2', '/plugins', array(
-		    'methods' => 'POST',
-		    'callback' => 'rest_post_plugins',
-		    /*'permission_callback' => function () {
-		        return current_user_can( 'activate_plugins' );
-		    }*/
-	  ) );
-} );
+class Proudpack {
 
-function rest_get_plugins() {
-		// Check if get_plugins() function exists. This is required on the front end of the
-		// site, since it is in a file that is normally only loaded in the admin.
-		if ( ! function_exists( 'get_plugins' ) ) {
-				require_once ABSPATH . 'wp-admin/includes/plugin.php';
-		}
+  public function __construct() {
+    $this->rest_router();
 
-		return get_plugins();
-}
+    $this->options = [
+      'blogname',
+      'blogdescription',
 
-function rest_post_plugins() {
-		return($_REQUEST);
-		if ($_REQUEST['action'] == 'activate') {
-				activate_plugins($_REQUEST['plugins']);
-		}
-		else {
-				deactivate_plugins($_REQUEST['plugins']);
-		}
-}
+      'city',
+      'state',
+      'lat',
+      'lng',
+
+      'external_link_window',
+      'agency_label',
+
+      'google_analytics_key',
+
+      'search_service',
+      'search_google_key',
+
+      'payment_service',
+      'payment_stripe_type',
+      'payment_stripe_key',
+      'payment_stripe_secret',
+
+      '311_service',
+      '311_link_create',
+      '311_link_status',
+
+      'mapbox_token',
+      'mapbox_map',
+
+      'embed_code',
+      'validation_metatags',
+
+      'alert_active',
+      'alert_message',
+      'alert_severity',
+    ];
+  }
+
+  public function rest_router() {
+    add_action( 'rest_api_init', function () {
+      /*register_rest_route( 'wp/v2', '/menu', array(
+        'methods' => 'GET',
+        'callback' => [$this, 'rest_get_plugins'],
+        /*'permission_callback' => function () {
+          return current_user_can( 'activate_plugins' );
+        }
+      ) );*/
+      register_rest_route( 'wp/v2', '/plugins', array(
+        'methods' => 'GET',
+        'callback' => [$this, 'rest_get_plugins'],
+        'permission_callback' => function () {
+          return current_user_can( 'activate_plugins' );
+        }
+      ) );
+      register_rest_route( 'wp/v2', '/plugins', array(
+        'methods' => 'POST',
+        'callback' => [$this, 'rest_post_plugins'],
+        'permission_callback' => function () {
+          return current_user_can( 'activate_plugins' );
+        }
+      ) );
+      register_rest_route( 'wp/v2', '/options', array(
+        'methods' => 'GET',
+        'callback' => [$this, 'rest_get_options'],
+        'permission_callback' => function () {
+          return current_user_can( 'edit_proud_options' );
+        }
+      ) );
+      register_rest_route( 'wp/v2', '/options', array(
+        'methods' => 'POST',
+        'callback' => [$this, 'rest_post_options'],
+        'permission_callback' => function () {
+          return current_user_can( 'edit_proud_options' );
+        }
+      ) );
+    } );
+  }
+  
+  public function rest_get_plugins() {
+    // Check if get_plugins() function exists. This is required on the front end of the
+    // site, since it is in a file that is normally only loaded in the admin.
+    if ( ! function_exists( 'get_plugins' ) ) {
+      require_once ABSPATH . 'wp-admin/includes/plugin.php';
+    }
+
+    $plugins = get_plugins();
+    foreach ($plugins as $key => $plugin) {
+      $plugins[$key]['Active'] = is_plugin_active($key);
+    }
+
+    return $plugins;
+  }
+
+  public function rest_post_plugins() {
+    if ($_POST['action'] == 'activate') {
+      activate_plugin($_POST['plugin']);
+    }
+    else {
+      deactivate_plugins($_POST['plugin']);
+    }
+    return($_POST);
+  }
 
 
-function get_rest_plugins() {
-   // Get the nav menu based on $menu_name (same as 'theme_location' or 'menu' arg to wp_nav_menu)
+  public function get_rest_plugins() {
+     // Get the nav menu based on $menu_name (same as 'theme_location' or 'menu' arg to wp_nav_menu)
     // This code based on wp_nav_menu's code to get Menu ID from menu slug
 
     $menu_name = 'custom_menu_slug';
 
     if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
-	$menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+    $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
 
-	$menu_items = wp_get_nav_menu_items($menu->term_id);
+    $menu_items = wp_get_nav_menu_items($menu->term_id);
 
-	$menu_list = '<ul id="menu-' . $menu_name . '">';
+    $menu_list = '<ul id="menu-' . $menu_name . '">';
 
-	foreach ( (array) $menu_items as $key => $menu_item ) {
-	    $title = $menu_item->title;
-	    $url = $menu_item->url;
-	    $menu_list .= '<li><a href="' . $url . '">' . $title . '</a></li>';
-	}
-	$menu_list .= '</ul>';
+    foreach ( (array) $menu_items as $key => $menu_item ) {
+      $title = $menu_item->title;
+      $url = $menu_item->url;
+      $menu_list .= '<li><a href="' . $url . '">' . $title . '</a></li>';
+    }
+    $menu_list .= '</ul>';
     } else {
-	$menu_list = '<ul><li>Menu "' . $menu_name . '" not defined.</li></ul>';
+    $menu_list = '<ul><li>Menu "' . $menu_name . '" not defined.</li></ul>';
     }
     // $menu_list now ready to output
-}
+  }
+
+  public function rest_get_options() {
+    $return = [];
+    foreach ($this->options as $option) {
+      $return[$option] = get_option($option);
+    }
+    return $return;
+  }
+
+  public function rest_post_options($data) {
+    $out = [];
+    foreach ($_POST as $key => $value) {
+      if (in_array($key, $this->options)) {
+        //@todo: sanitization
+        update_option($key, $value);
+        $out[$key] = $value;
+      }
+    }
+    return $out;
+  }
+
+
+} // Class
+
+
+new Proudpack();
